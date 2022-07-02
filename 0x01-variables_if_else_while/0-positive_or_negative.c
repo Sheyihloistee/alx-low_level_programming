2dad291a401beaef721931421672c8ae8ea1905d
Adekoya_Oluwaseyi_Taiwo <aseyitaiwo@gmail.com>
2022-07-02 01:53:33 -0700

    assign a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,18 +5,18 @@
  * main - assigns a random number to the value of n everytime
  * it executes and prints it
  * Return: 0 if (successful)
- */
+*/
 int main(void)
 {
-int n;
+ int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
- printf("%d is positive\n", n);
+ srand(time(0));
+ n = rand() - RAND_MAX / 2;
+ if (n > 0)
+  printf("%d is positive\n", n);
  else if (n == 0)
- printf("%d is zero\n", n);
+  printf("%d is zero\n", n);
  else if (n < 0)
- printf("%d is negative\n", n);
+  printf("%d is negative\n", n);
  return (0);
 }
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,3 @@
+0.This program will assign a random number to the variable n each time it is executed. 
+1. A program will assign a random number to the variable n each time it is executed.
+2. A program that prints the alphabet in lowercase, followed by a new line
