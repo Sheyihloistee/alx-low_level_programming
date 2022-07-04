0a45429ccf71ad8e73fd1a0f5dd3082f5a4c2394
Adekoya_Oluwaseyi_Taiwo <aseyitaiwo@gmail.com>
2022-07-04 11:24:31 -0700

    a program that prints the lowercase alphabet in reverse, followed by a new line

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-                                                                 
-/**            
- * main - prints numbers of base 16 in lowercase                   
+                                                               /**            
+ * main - prints numbers of base 16 in lowercase              
  * followed by  new line                    
- * Return: 0 if (successful)                                      
-*/
+ * Return: 0 if (successful)                                   */
 
 int main(void)
-n;
+{
+int n;
 char ch;
 
-for (n = 48; n < 58; n++)                                         
-{         
+for (n = 48; n < 58; n++)                                      {         
 putchar(n);
 }       
 for (ch = 'a'; ch <= 'f'; ch++)
