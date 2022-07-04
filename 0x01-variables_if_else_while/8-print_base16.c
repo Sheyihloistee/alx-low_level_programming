c2b3b2b987951108d36cb58868a76fb726d6e4f0
Adekoya_Oluwaseyi_Taiwo <aseyitaiwo@gmail.com>
2022-07-04 11:03:20 -0700

    a program that prints all the numbers of base 16 in lowercase, followed by a new line

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
                                                                  
 /**            
- * main - prints numbers of base 16 in lowercase                   * followed by  new line                    
- * Return: 0 if (successful)                                      */      
-                                                               
+ * main - prints numbers of base 16 in lowercase                   
+ * followed by  new line                    
+ * Return: 0 if (successful)                                      
+*/
+
 int main(void)
-{               
-int n;
+n;
 char ch;
 
 for (n = 48; n < 58; n++)                                         
