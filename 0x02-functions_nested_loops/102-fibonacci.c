f94b7b1b766012dcac8a185f8ed8c5aa8de6938f
Adekoya_Oluwaseyi_Taiwo <aseyitaiwo@gmail.com>
2022-07-06 13:58:25 -0700

    a program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,22 +5,26 @@
  * Description: prints even numbers less than 4,000,000
  * followed by a new line
  * Return: 0
-*/
+ */
 
 int main(void)
 {
-int a = 0, b = 1, next = 0;
-int sum = 0;
+int i = 0;
+long int a = 0, b = 1, next;
 
-while (next < 4000000)
+while (i < 50)
 {
 next = a + b;
 a = b;
 b = next;
-if (next % 2 == 0)
-sum += next;
+printf("%lu", next);
+
+if (i < 49)
+{
+printf(",");
 }
-printf("%i\n", sum);
+i++;
+}
+putchar('\n');
 return (0);
 }
-
