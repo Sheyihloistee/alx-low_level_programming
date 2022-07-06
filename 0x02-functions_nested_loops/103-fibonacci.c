8c39528bc8e532e2f27c0cba229fb1c05305017e
Adekoya_Oluwaseyi_Taiwo <aseyitaiwo@gmail.com>
2022-07-06 14:36:30 -0700

    a program that finds and prints

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,46 +2,25 @@
 
 /**
  * main - main block
- * Description: prints the first 98 fibonacci numbers starting with 1 and 2
+ * Description: prints even numbers less than 4,000,000
  * followed by a new line
  * Return: 0
-*/
+ */
 
 int main(void)
 {
-unsigned long int i;
-unsigned long int bef = 1;
-unsigned long int aft = 2;
-unsigned long int l = 1000000000;
-unsigned long int bef1;
-unsigned long int bef2;
-unsigned long int aft1;
-unsigned long int aft2;
+	int a = 0, b = 1, next = 0;
+	int sum = 0;
 
-printf("%lu", bef);
-
-for (i = 1; i < 91; i++)
-{
-printf(", %lu", aft);
-aft += bef;
-bef = aft - bef;
-}
-
-bef1 = (bef / l);
-bef2 = (bef % l);
-aft1 = (aft / l);
-aft2 = (aft % l);
-
-for (i = 92; i < 99; ++i)
-{
-printf(", %lu", aft1 + (aft2 / l));
-printf("%lu", aft2 % l);
-aft1 = aft1 + bef1;
-bef1 = aft1 - bef1;
-aft2 = aft2 + bef2;
-bef2 = aft2 - bef2;
-}
-printf("\n");
-return (0);
+	while (next < 4000000)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		if (next % 2 == 0)
+			sum += next;
+	}
+	printf("%i\n", sum);
+	return (0);
 }
 
