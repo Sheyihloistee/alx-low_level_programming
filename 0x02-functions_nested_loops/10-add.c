dfbfc4da2979ea281763e75326cef8574e078511
Adekoya_Oluwaseyi_Taiwo <aseyitaiwo@gmail.com>
2022-07-05 23:37:45 -0700

    a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - returns the sum of  the given parameters
+ * @a: int type number
+ * @b: int type number
+ * Return: 0
+*/
+
+int add(int a, int b)
+{
+return (a + b);
+}
+
