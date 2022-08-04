9ee911efcf77cef22209fcb2d89087411b46ea3e
Adekoya_Oluwaseyi_Taiwo <aseyitaiwo@gmail.com>
2022-08-04 11:43:06 -0700

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,8 +10,8 @@ void print_all(const char * const format, ...);
 
 /**
  * print_char - Prints a char.
- * @arg: A list of arguments pointing to 
- * the character to be printed.
+ * @arg: A list of arguments pointing to
+ *       the character to be printed.
  */
 void print_char(va_list arg)
 {
